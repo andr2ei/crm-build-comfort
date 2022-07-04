e2c3d93212aa5bebbba5b6a64b0715e696e9990c
Andrei Andronov <ao.andronov@gmail.com>
2022-07-04 20:30:09 +0300

    feat: adding commercial proposal report

--- src/main/java/ru/andronov/crm/controller/ProductController.java
+++ src/main/java/ru/andronov/crm/controller/ProductController.java
@@ -1,11 +1,18 @@
 package ru.andronov.crm.controller;
 
+import com.lowagie.text.DocumentException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.andronov.crm.domain.Product;
 import ru.andronov.crm.service.IProductService;
+import ru.andronov.crm.utils.ProductPDFExporter;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -42,5 +49,24 @@ public class ProductController {
     @DeleteMapping(path = "product/{id}")
     public void updateProduct(@PathVariable("id") int id) {
         productService.deleteById(id);
+    }
+
+    @GetMapping("product/export/pdf/{lead_id}")
+    public void exportToPDF(@PathVariable("lead_id") int leadId,
+                            @RequestParam(name = "discount", defaultValue = "0") int discount,
+                            HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=products_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<Product> products = productService.getProductsByLeadId(leadId);
+
+        var exporter = new ProductPDFExporter(products, discount);
+        exporter.export(response);
+
     }
 }
---
+++ src/main/java/ru/andronov/crm/utils/ProductPDFExporter.java
@@ -0,0 +1,90 @@
+package ru.andronov.crm.utils;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.*;
+import ru.andronov.crm.domain.Product;
+
+
+public class ProductPDFExporter {
+    private final List<Product> products;
+    private final int discount;
+
+    public ProductPDFExporter(List<Product> products, int discount) {
+        this.products = products;
+        this.discount = discount;
+    }
+
+    private void writeTableHeader(PdfPTable table) {
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.LIGHT_GRAY);
+        cell.setPadding(2);
+
+        Font font = FontFactory.getFont(FontFactory.HELVETICA);
+        font.setColor(Color.GREEN);
+
+        cell.setPhrase(new Phrase("Товар"));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Комментарий"));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Кол-во"));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Цена"));
+        table.addCell(cell);
+        cell.setPhrase(new Phrase("Стоимосить"));
+        table.addCell(cell);
+    }
+
+    private void writeTableData(PdfPTable table) {
+        for (Product product : products) {
+            table.addCell(product.getName());
+            table.addCell(product.getComment());
+            table.addCell(String.valueOf(product.getCount()));
+            table.addCell(String.valueOf(product.getPrice()));
+            table.addCell(String.valueOf(product.getCount() * product.getPrice()));
+        }
+    }
+
+    private double countTotalCost() {
+        var total = products.stream()
+                .map(p -> p.getPrice() * p.getCount())
+                .reduce(0.0, Double::sum);
+        return Math.round(total * 100) / 100.0;
+    }
+
+    private double countTotalCostWithDiscount() {
+        return Math.round(countTotalCost() * (1 - discount / 100.0) * 100) / 100.0;
+    }
+
+    public void export(HttpServletResponse response) throws DocumentException, IOException {
+        try (Document document = new Document(PageSize.A4)) {
+            PdfWriter.getInstance(document, response.getOutputStream());
+
+            document.open();
+
+            var firstPar = new Paragraph("Коммерческое предложение");
+            firstPar.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(firstPar);
+
+            PdfPTable table = new PdfPTable(5);
+            table.setWidthPercentage(100f);
+            table.setWidths(new float[]{2f, 3.5f, 1.5f, 3.0f, 3.0f});
+            table.setSpacingBefore(10);
+            writeTableHeader(table);
+            writeTableData(table);
+            document.add(table);
+
+            var totalCost = countTotalCost();
+            var totalCostWithDiscount = countTotalCostWithDiscount();
+            var lastPar = new Paragraph("Итого " + totalCost + ". Итого со скидкой " + discount + "% " + totalCostWithDiscount);
+            lastPar.setAlignment(Paragraph.ALIGN_CENTER);
+            document.add(lastPar);
+        }
+    }
+}
